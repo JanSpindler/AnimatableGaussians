dc612c4cba8dddc256c1a3968dcf9bb057c6b5ee
Jan Spindler <jspindle@uni-bonn.de>
2026-02-27 11:13:49 +0100

    updated install script

--- install_ub.sh
+++ install_ub.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-conda install nvidia::cuda-toolkit==12.8.1
+conda install nvidia::cuda-toolkit==12.8.1 -y
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu128
 pip install glfw libigl joblib numpy opencv-python plyfile PyOpenGL pyrender pyyaml scikit-image scikit-learn screeninfo setuptools tqdm trimesh
 export TORCH_CUDA_ARCH_LIST="12.0"
+pip install --no-build-isolation "git+https://github.com/facebookresearch/pytorch3d.git"
